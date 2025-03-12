e2c3fcca87bb9675be0c8bc5a515e7f355289ca7
Dcodigo1Max <davidrasinhas.info@gmail.com>
2025-03-12 23:12:44 +0000

    Adicionar XML aos metodos Main e Decor

--- BetterDecorador/Program.cs
+++ BetterDecorador/Program.cs
@@ -7,6 +7,15 @@ namespace BetterDecorador
 {
     public class Program
     {
+        /// <summary>
+        /// Este codigo vai fazer um ciclo utilizando os caracteres atribuidos mais 
+        /// outros valores para poder contar o numero de vezes que o caracter aparece
+        /// de acordo com o input que o utilizador escreve na consola
+        /// </summary>
+        /// <param name="s">variavel tipo string da frase que vai ser imprimida</param>
+        /// <param name="dec">Caracteres que aparecem no inicio e no fim da string</param>
+        /// <param name="numbers">variavel que tem o numero de vezes que um caracter é inserido</param>
+        /// <returns></returns>
         private static string Decor(string s, char dec, int numbers)
             {
 
@@ -27,8 +36,12 @@ namespace BetterDecorador
             
             }
 
-       
-       
+        /// <summary>
+        /// Imprime o Metodo Decor de acordo com os parametros fornecidos
+        ///  e faz a conversão das variaveis necessarias para string
+        /// </summary>
+        /// <param name="args"></param>
+
         private static void Main(string[] args)
         {
 
