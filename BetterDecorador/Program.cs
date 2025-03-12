e036d4db2d0f2d4ab6afcc4c1fe0cbb3098083c5
Dcodigo1Max <davidrasinhas.info@gmail.com>
2025-03-12 22:50:14 +0000

    Ex1 concluido

--- BetterDecorador/Program.cs
+++ BetterDecorador/Program.cs
@@ -1,22 +1,43 @@
 ﻿using System;
+using System.Globalization;
 using System.Runtime.CompilerServices;
+using System.Threading;
 
 namespace BetterDecorador
 {
     public class Program
     {
-        private static string Decor(string s, char dec)
+        private static string Decor(string s, char dec, int numbers)
             {
-                return $"{dec}{dec}{dec} {s} {dec}{dec}{dec} "; 
+
+                int n = 0;
+                s= " " + s + " ";
+
+                while(n !=numbers)
+                {
+                    s = dec + s + dec;
+                    ++n;
+                }
+                return s;
+                
+                
+                //return $"{Convert.ToString(dec)}{Convert.ToString(dec)}{Convert.ToString(dec)}{s} {Convert.ToString(dec)}{Convert.ToString(dec)}{Convert.ToString(dec)}{Convert.ToString(numbers)}"; 
+                
+                 
+            
             }
 
        
        
         private static void Main(string[] args)
         {
+
+            Console.WriteLine(Decor(args[0], char.Parse(args[1]), int.Parse(args[2])));
            
-            Console.WriteLine(Decor("Good Morning!", '*'));
-            Console.WriteLine(Decor("Good Evening!", '.'));
+
+
+            //Console.WriteLine(Decor("Good Morning!", '*', ));
+            //Console.WriteLine(Decor("Good Evening!", '.', ));
 
 
         }
